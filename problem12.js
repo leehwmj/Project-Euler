7939e11ae7b0d0b6acf52467bad1d2cf2696ea38
MooJu <leehwmj@core.today>
2017-10-04 01:48:57 +0900

    12번 문제 중간 점검.
    약수의 갯수를 확인하는 로직과 삼각수를 구하는 로직을 각각 만들었음.
    이제 삼각수를 받아서 약수의 갯수를 확인하게 코드를 바꿔야 함.
    고민중..

--- problem12.js
+++ problem12.js
@@ -2,3 +2,29 @@
 
 // 1. n번째의 삼각수를 구하는 로직
 // 2. 위에서 구해진 삼각수를 받아서 약수의 갯수를 확인하는 로직
+
+// 약수의 갯수를 확인 하는 로직
+function counter(number) {
+    let counting = 0;
+    for (let i = 1; i <= number; i++){
+        if(number%i===0) {
+            counting ++
+        }
+    }
+    return counting
+}
+
+// n 번째 삼각수를 구하는 로직
+// 1
+// 1+2
+// 1+2+3
+// 1+2+3+4
+// 1+2+3+4+5
+let triangular = 0;
+for (let i = 1; i <= 100; i++){
+    triangular += i
+}
+// console.log(counter(triangular))
+
+
+console.log(counter(5050))
